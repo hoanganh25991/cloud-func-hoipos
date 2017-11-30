0d3e3a85070b843ab2d41821004e8494c1f441f2
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-30 15:04:12 +0700

    Add cmt

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 import axios from "axios"
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
 import config from "config.json"
+const functions = require("firebase-functions")
+const admin = require("firebase-admin")
 
+// Config
 const _ = console.log
 axios.defaults.timeout = 2000
-const {hoiPosEndpoint, orderVersion} = config
-admin.initializeApp(functions.config().firebase);
+const { hoiPosEndpoint, orderVersion } = config
+admin.initializeApp(functions.config().firebase)
 
 /**
  * Call save orders in hoipos backend
  * To save it to MySQl db
  * @type {*|CloudFunction<DeltaSnapshot>}
  */
-export const callUploadOrder = functions.database.ref('/tmp/{outletBr}/{orderBr}').onWrite(async event => {
+export const callUploadOrder = functions.database.ref("/tmp/{outletBr}/{orderBr}").onWrite(async event => {
   _("[event data]", event.data.val())
 
   // Check if should handle event
-  const {outletBr, orderBr} = event.params
+  const { outletBr, orderBr } = event.params
   _("[ouletId, orderBr]", outletBr, orderBr)
   const matchOutletBr = outletBr.match(/^.+(\d+)_orders$/)
-  const matchOrderX   = orderBr.match(/^order_(\d+)/)
-  const shouldHandle  = matchOutletBr && matchOrderX && true
+  const matchOrderX = orderBr.match(/^order_(\d+)/)
+  const shouldHandle = matchOutletBr && matchOrderX && true
   _("[shouldHandle]", shouldHandle)
-  if(!shouldHandle) return null
+  if (!shouldHandle) return null
 
   // Call hoipos-backend > uploadOrders API
   const orderData = event.data.val()
@@ -32,10 +33,10 @@ export const callUploadOrder = functions.database.ref('/tmp/{outletBr}/{orderBr}
   const outlet_id = +matchOutletBr[1]
 
   const res = await axios.post(hoiPosEndpoint, {
-    type:"SAVE_ORDERS",
-    orders:[orderData],
+    type: "SAVE_ORDERS",
+    orders: [orderData],
     outlet_id
   })
 
   return null
-});
+})
