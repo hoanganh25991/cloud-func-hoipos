dd3da3ad31532e8a764cd386cac1e358e8f80015
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-11-30 12:19:28 +0700

    Deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+import fs from "fs"
+import cpr from "child_process"
+const _ = console.log
+const {dependencies} = require(`${__dirname}/../package.json`)
+const defaultPkg = require(`${__dirname}/defaultPkg.json`)
+
+_("[INFO] Create pkg")
+const pkg = {...defaultPkg, dependencies}
+const pkgPath = `${__dirname}/../functions/package.json`
+fs.writeFileSync(pkgPath, JSON.stringify(pkg))
+
+_("[INFO] Build")
+const buildLog = cpr.execSync(`yarn build`)
+_(buildLog.toString())
+
+_("[INFO] Deploy")
+const cmd = [  `cd functions`,  `yarn install`,  `yarn deploy`]
+const deployLog = cpr.execSync(cmd.join("&&"))
+_(deployLog.toString())
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const _ = console.log
 
 admin.initializeApp(functions.config().firebase);
 
-export const callUploadOrder = functions.database.ref('/tmp').onWrite(event => {
-  console.log("[event data]", event.data.val())
+export const callUploadOrder = functions.database.ref('/tmp/{outletId}/{orderId}').onWrite(event => {
+  // Check if should handle event
+  const {outletId, orderId} = event.params
+  _("[ouletId, orderId]", outletId, orderId)
+  _("[event data]", event.data.val())
+
+
+  const matchOrderBrand = outletId.match(/^.+(\d+)_orders$/)
+  const matchOrderX     = orderId.match(/^order_(\d+)/)
+
+  const shouldHandle = matchOrderBrand && matchOrderX
+
+  if(!shouldHandle) return null
+
+  _("[shouldHandle]", shouldHandle)
+
+  const orderData = event.data.val()
+  _("[orderData]", orderData)
+
+  // Call hoipos-backend > uploadOrders API
+
   return null
 });
