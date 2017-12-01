174a3fb2e7fdb42792a32383230e63c6ad01b112
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-01 10:17:07 +0700

    Deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,52 @@
 import fs from "fs"
 import cpr from "child_process"
+
+// import config
+import config from "../src/config.json"
+import { dependencies } from "../package.json"
+import defaultPkg from "defaultPkg.json"
+
 const _ = console.log
-const { dependencies } = require(`${__dirname}/../package.json`)
-const defaultPkg = require(`${__dirname}/defaultPkg.json`)
-
 const args = process.argv.slice(2)
-// const p
 
-_("[INFO] Create pkg")
-const pkg = { ...defaultPkg, dependencies }
-const pkgPath = `${__dirname}/../functions/package.json`
-fs.writeFileSync(pkgPath, JSON.stringify(pkg))
+const run = ({ config, defaultPkg, dependencies }) => {
+  const projectName = args[0] || "hoidev"
+  const projectId = Object.keys(config).filter(key => {
+    const project = config[key]
+    if (typeof project !== "object") return false
+    return project.name === projectName
+  })[0]
 
-_("[INFO] Copy config")
-const configLog = cpr.execSync(`cp ${__dirname}/../src/config.json ${__dirname}/../functions`)
-_(configLog.toString())
+  _(`[INFO] Project Id: ${projectId}`)
+  if (!projectId) return
 
-_("[INFO] Build")
-const buildLog = cpr.execSync(`yarn build`)
-_(buildLog.toString())
+  _("[INFO] Copy config")
+  const configLog = cpr.execSync(`cp ${__dirname}/../src/config.json ${__dirname}/../functions`)
+  _(configLog.toString())
 
-_("[INFO] Deploy")
-const cmd = [`cd functions`, `yarn install`, `yarn deploy:prod`]
-const deployLog = cpr.execSync(cmd.join("&&"))
-_(deployLog.toString())
+  _("[INFO] Create pkg")
+  const pkg = {
+    ...defaultPkg,
+    dependencies,
+    scripts: {
+      deploy: `firebase use ${projectId} && firebase deploy --only functions`
+    }
+  }
+  const pkgPath = `${__dirname}/../functions/package.json`
+  fs.writeFileSync(pkgPath, JSON.stringify(pkg))
+
+  _("[INFO] Build")
+  const buildLog = cpr.execSync(`yarn build`)
+  _(buildLog.toString())
+
+  _("[INFO] ChDir & Deploy")
+  const cmd = [`cd functions`, `yarn install`, `yarn deploy`]
+  const deployLog = cpr.execSync(cmd.join("&&"))
+  _(deployLog.toString())
+}
+
+try {
+  run({ config, defaultPkg, dependencies })
+} catch (err) {
+  _("[run ERR]", err)
+}
